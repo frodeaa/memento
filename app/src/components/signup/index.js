cac77e0de6bb76c4e6887cbc8abc9ab6896ced93
Frode Aannevik <frode.aa@gmail.com>
2017-02-11 14:45:43 +0100

    complete sign-up form inputs
    sign up with name, email and password

--- app/src/components/signup/index.js
+++ app/src/components/signup/index.js
@@ -9,7 +9,22 @@ export default class Signup extends Component {
 					<h1>Create a new account</h1>
 				</header>
 				<form>
-
+					<field>
+						<label for="signup_name">Name</label>
+						<input name="signup_name" type="text" required></input>
+	  				</field>
+	  				<field>
+						<label for="signup_email">Email</label>
+						<input name="signup_email" type="email"></input>
+	  				</field>
+	  				<field>
+						<label for="signup_password">Password</label>
+						<input name="signup_password" type="password"></input>
+	  				</field>
+	  				<field>
+	  					<input name="signup_commit" type="submit"
+	  						value="Sign Up"></input>
+					</field>
 				</form>
 			</main>
 		);

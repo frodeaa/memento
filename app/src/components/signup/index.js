81b5870332276b50b3dbb51ae4a82e33db9ce3aa
Frode Aannevik <frode.aa@gmail.com>
2017-02-11 13:32:29 +0100

    start on signup component

---
+++ app/src/components/signup/index.js
@@ -0,0 +1,17 @@
+import { h, Component } from 'preact';
+import style from './style';
+
+export default class Signup extends Component {
+	render() {
+		return (
+			<main class={style.signup}>
+				<header>
+					<h1>Create a new account</h1>
+				</header>
+				<form>
+
+				</form>
+			</main>
+		);
+	}
+}

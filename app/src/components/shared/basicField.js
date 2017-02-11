5eb016115284861d429538d52caddfc2ed9337b2
Frode Aannevik <frode.aa@gmail.com>
2017-02-11 17:52:23 +0100

    get form value from inputs

--- app/src/components/shared/basicField.js
+++ app/src/components/shared/basicField.js
@@ -1,14 +1,13 @@
 import { h, Component } from 'preact'
 
 export default class Field extends Component {
-  render(props, label) {
+  render(props, onChange) {
     return (
       <field>
         <label for={props.name}>{props.label}</label>
         <input 
           type={props.type}
-          name={props.name}
-          required></input>
+          name={props.name} onChange={props.onChange}></input>
       </field>
     );
   }
--- app/src/components/signup/index.js
+++ app/src/components/signup/index.js
@@ -3,6 +3,21 @@ import style from './style';
 import Field from '../shared/basicField.js';
 
 export default class Signup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { };
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+
   render() {
     return (
       <main class={style.signup}>
@@ -10,11 +25,14 @@ export default class Signup extends Component {
           <h1>Create a new account</h1>
         </header>
         <form>
-          <Field type="text" name="signup_name" label="Name" required></Field>
-          <Field type="email" name="signup_email" label="Email"></Field>
-          <Field type="password" name="signup_password" label="Password"></Field>
+          <Field type="text" name="username" label="Name"
+            onChange={this.handleInputChange}></Field>
+          <Field type="email" name="email" label="Email"
+            onChange={this.handleInputChange}></Field>
+          <Field type="password" name="password" label="Password"
+            onChange={this.handleInputChange}></Field>
           <field>
-            <input name="signup_commit" type="submit"
+            <input name="commit" type="submit"
               value="Sign Up"></input>
           </field>
         </form>
